5c7e0b4fefd6e72b4ca663fd7a72450d34974551
paolino <paolo.veronelli@gmail.com>
2016-05-05 11:45:54 +0200

    eliminated listWithKey use

--- Client.hs
+++ Client.hs
@@ -29,18 +29,6 @@ import Data.GADT.Compare
 (<$$) :: (Functor f, Functor g) => a -> f (g b) -> f (g a)
 (<$$) f = ((f <$) <$>)
 
----------------- order preserving map ------------------
-type MM k a = M.Map (Int,k) a
-
--- add a new value to a map, automatically choosing an unused key
-new :: Ord k => (k,a) -> MM k a -> MM k a
-new (k,v) m = case M.maxViewWithKey m of
-    Nothing -> M.fromList [((0,k),v)] -- overloadedlists
-    Just (((n,_), _), _) -> M.insert (succ n,k) v m
-
-mmAssocs :: MM k a -> [(k,a)]
-mmAssocs = map (first snd) . M.assocs
-
 ------------------ radio checkboxes ----------------------
 --
 radiocheckW :: (MonadHold t m,MonadWidget t m) => Eq a => a -> [(String,a)] -> m (Event t a)
@@ -73,7 +61,6 @@ inputW :: MonadWidget t m => m (Event t String)
 inputW = do
     rec let send = ffilter (==13) $ view textInput_keypress input -- send signal firing on *return* key press
         input <- textInput $ def & setValue .~ fmap (const "") send -- textInput with content reset on send
-        domEvent Focus
     return $ tag (current $ view textInput_value input) send -- tag the send signal with the inputText value BEFORE resetting
 
 selInputW
@@ -94,6 +81,9 @@ linkNewTab href s = elAttr "a" ("href" =: href <> "target" =: "_blank") $ text s
 
 joinE :: (Reflex t, MonadHold t f) => Event t (Event t a) -> f (Event t a)
 joinE = fmap switch . hold never
+
+dynamicE  :: MonadWidget t m => (a -> m (Event t a1)) -> Dynamic t a -> m (Event t a1)
+dynamicE f d = mapDyn f d >>= dyn >>= joinE
 
 --------- buggy namings, wait for Dynamic functor instance ---------------
 combineDynWith = Reflex.combineDyn
@@ -132,7 +122,7 @@ record = do
 type Button = (String, EC)
 
 ---- button definitions as a map (the only acceptable structure for Reflex.DOM ?)
-type ButtonsDefs =  MM String EC
+type ButtonsDefs =  [Button] --MM String EC
 
 ------------------- the beta reduction widget ----------------
 --
@@ -175,7 +165,7 @@ reductionW (ReductionInput red dbm p) = maybe never id <$> case parsing p of
             (bs,es) <- fmap unzip . elClass "ol" "steps" $ 
                 forM (withFreshes var_names $ betas red e) $ \r -> do 
                     elClass "li" "steps"  $ do
-                        text . pprintdb (maybe [] mmAssocs dbm) $ r
+                        text . pprintdb (maybe [] id dbm) $ r
                         b <- elClass "span" "up" $ button "edit"
                         return (r, fmap (const r) b)
             s <- record          
@@ -191,7 +181,7 @@ var_names = nub $ "xyzwnmlkij" ++ ['a' .. 'z']
 
 boot :: ButtonsDefs
 
-boot = foldr new M.empty $ [
+boot = [
     ("FALSE",false var_names),
     ("TRUE",true var_names),
     ("AND",and_ var_names),
@@ -203,22 +193,28 @@ boot = foldr new M.empty $ [
     ] 
 
 --------------------- group button widget --------------------------
-
-
+{-
+button' :: MonadWidget t m => String -> m (Event t ())
+button' s = do
+  (e, _) <- elAttr' "button" (Map.singleton "type" "button") $ text s
+  return $ domEvent Click e
+-}
 -- expression elements -----------------------------------------
 extrabuttons :: MS m => m [ES String]
 extrabuttons = mapM (\c -> c <$$ button c) ["(" ,"(λ","x" ,"y" ,"z" ,"w" ,"n" ,"m" ,"l" ,"." ,")"] 
 
 -- make a button firing an expression
-makeButton :: MS m => String -> DS EC -> m (ES String)
-makeButton k v = fmap (pprint . fst) <$> attachDyn v <$> button k
+makeButton :: MS m => String -> EC -> m (ES String)
+makeButton k v = do
+    fmap (pprint . fst) <$> fmap (v ,) <$> button k
+--
 --
 buttonsW :: (MonadWidget Spider m) => DS ButtonsDefs -> m (ES String)
 buttonsW buttonsDef = divClass "standard" $ do 
     
     keys <- extrabuttons
-    dynOfEvents :: DS (ES String) <- listWithKey buttonsDef (makeButton . snd) >>= mapDyn (leftmost . M.elems)           
-    return . leftmost $ (switch . current $ dynOfEvents) : keys
+    e :: ES String <- dynamicE (fmap leftmost . mapM (uncurry makeButton)) buttonsDef          
+    return . leftmost $ e : keys
 
 ----------------- the expression field widget ---------------
 --
@@ -256,14 +252,14 @@ main = mainWidget . void $ do
               f <- combineDynWith ReductionInput redType bf
               combineDynWith ($) f expression
             
-        reductionE :: ES (DMap ReductionE Identity) <- mapDyn reductionW bdefsAndExpr >>= dyn >>= joinE
+        reductionE :: ES (DMap ReductionE Identity) <- dynamicE reductionW bdefsAndExpr 
         
         let     newButton = pick NewButton reductionE
                 upEC = pick NewEdit reductionE
 
         redType <- holdDyn Normal $ pick ChangeTactic reductionE
 
-        buttonsDef :: DS ButtonsDefs  <- foldDyn new boot newButton
+        buttonsDef :: DS ButtonsDefs  <- foldDyn (flip (++) . return) boot newButton
 
     footer
 {-
