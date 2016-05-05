c3314af608799989dd182d74dafc3aaae48c2db9
paolino <paolo.veronelli@gmail.com>
2016-05-05 11:26:03 +0200

    swith exp

--- Client.hs
+++ Client.hs
@@ -1,5 +1,5 @@
 
-{-# LANGUAGE RecursiveDo,StandaloneDeriving, NoMonomorphismRestriction, TupleSections, FlexibleContexts,ScopedTypeVariables,OverloadedLists, ConstraintKinds, TemplateHaskell #-}
+{-# LANGUAGE RecursiveDo,StandaloneDeriving, NoMonomorphismRestriction, TupleSections, FlexibleContexts,ScopedTypeVariables, ConstraintKinds, TemplateHaskell #-}
 {-# LANGUAGE GADTs#-}
 import Data.Dependent.Map (DMap, DSum( (:=>) ) , fromList)
 import Data.GADT.Compare.TH
@@ -35,7 +35,7 @@ type MM k a = M.Map (Int,k) a
 -- add a new value to a map, automatically choosing an unused key
 new :: Ord k => (k,a) -> MM k a -> MM k a
 new (k,v) m = case M.maxViewWithKey m of
-    Nothing -> [((0,k),v)] -- overloadedlists
+    Nothing -> M.fromList [((0,k),v)] -- overloadedlists
     Just (((n,_), _), _) -> M.insert (succ n,k) v m
 
 mmAssocs :: MM k a -> [(k,a)]
@@ -53,7 +53,7 @@ radiocheckW j xs = do
          result <- holdDyn j $ leftmost es
     return $ updated result
             
----------------- input widget -----------------------------------------------
+---------------- input widgets -----------------------------------------------
 insertAt :: Int -> String -> String -> (Int, String)
 insertAt n e s = let (u,v) = splitAt n s
                   in (n + length e, u ++ e ++ v)
@@ -73,6 +73,7 @@ inputW :: MonadWidget t m => m (Event t String)
 inputW = do
     rec let send = ffilter (==13) $ view textInput_keypress input -- send signal firing on *return* key press
         input <- textInput $ def & setValue .~ fmap (const "") send -- textInput with content reset on send
+        domEvent Focus
     return $ tag (current $ view textInput_value input) send -- tag the send signal with the inputText value BEFORE resetting
 
 selInputW
@@ -94,42 +95,38 @@ linkNewTab href s = elAttr "a" ("href" =: href <> "target" =: "_blank") $ text s
 joinE :: (Reflex t, MonadHold t f) => Event t (Event t a) -> f (Event t a)
 joinE = fmap switch . hold never
 
+--------- buggy namings, wait for Dynamic functor instance ---------------
 combineDynWith = Reflex.combineDyn
 combineDyn = combineDynWith (,)
 
-justThrough :: Reflex t => Event t (Maybe b) -> Event t b
-justThrough = fmapMaybe (fmap id)
-
----------------- these are wrong ,wait for Unalign instance ----------------------------
+---- select + fan combination, dismount a merge in one direction
 pick :: (GCompare k, Reflex t) => k a -> Event t (DMap k Identity) -> Event t a
 pick x r = select (fan r) x
-----------------------------------------------
------------------- End of libs---------------------
------------------------------------------------------
 
+---------------------------------------------------
+------------------ End of libs---------------------
+---------------------------------------------------
 
 
 ------------ noisy type and constraint renaming ---------
 type ES = Event Spider
 type DS = Dynamic Spider
 type MS = MonadWidget Spider
-
 ---------------------------------------------------
-var_names = nub $ "xyzwnmlkij" ++ ['a' .. 'z']
 
 
------------------- header ------------------------
+------------------ header -------------------------------------------------------------------------------
 header = elClass "h2" "head" $ do
-            divClass "matter" $ linkNewTab "https://en.wikipedia.org/wiki/Lambda_calculus" "Lambda Calculus"
-            divClass "source" $ linkNewTab "http://github.com/paolino/LambdaCalculus" "source code"
+    divClass "matter" $ linkNewTab "https://en.wikipedia.org/wiki/Lambda_calculus" "Lambda Calculus"
+    divClass "source" $ linkNewTab "http://github.com/paolino/LambdaCalculus" "source code"
 
 
 ------------- the storing new button widget -----------------
-record :: MS m => m (ES [Char])
+record :: MS m => m (ES String)
 record = do
-        s <- inputW
-        elClass "span" "tooltip" $ text "save to button"
-        return $ fmap (map toUpper) s 
+    s <- inputW
+    elClass "span" "tooltip" $ text "save to button"
+    return $ fmap (map toUpper) s 
 
 ----------------- a button is an equivalence relation betweena name and an expression
 type Button = (String, EC)
@@ -167,78 +164,83 @@ data ReductionInput = ReductionInput
 -- Second case the resuction widget is created and the ES DMap is returned
 reductionW :: MS m => ReductionInput -> m (ES (DMap ReductionE Identity))
 reductionW (ReductionInput red dbm p) = maybe never id <$> case parsing p of
-            Left e -> divClass "edit" $ do
-                        divClass "title" $ text "Not Parsed"
-                        elClass "span" "tooltip" $ text $ "Use λ or ! or  \\ or / or ^ to open a lambda"
-                        return Nothing
-            Right e -> do
-                divClass "edit" $ do
-                    divClass "title" $ do
-                                text "Beta reduction"
-                    r <- divClass "reduction_choice" $ radiocheckW red  [("aggressive",Aggressive) , ("mild",Mild), ("normal",Normal)] 
-                    (bs,es) <- fmap unzip . elClass "ol" "steps" $ 
-                        forM (withFreshes var_names $ betas red e) $ \r -> do 
-                             elClass "li" "steps"  $ do
-                                    text . pprintdb (maybe [] mmAssocs dbm) $ r
-                                    b <- elClass "span" "up" $ button "edit"
-                                    return (r, fmap (const r) b)
-                    s <- record          
-                    -- creation of the DMap ReductionE Identity tag :=> ES of the right type              
-                    let     b = NewButton :=> fmap (flip (,) (last bs)) s
-                            e = NewEdit :=> leftmost es
-                            r' = ChangeTactic :=> r
-                    return . Just . merge $ fromList [b,e,r']
+    Left e -> divClass "edit" $ do
+        divClass "title" $ text "Not Parsed"
+        elClass "span" "tooltip" . text $ "Use λ or ! or  \\ or / or ^ to open a lambda"
+        return Nothing
+    Right e -> do
+        divClass "edit" $ do
+            divClass "title" $ text "Beta reduction"
+            r <- divClass "reduction_choice" $ radiocheckW red  [("aggressive",Aggressive) , ("mild",Mild), ("normal",Normal)] 
+            (bs,es) <- fmap unzip . elClass "ol" "steps" $ 
+                forM (withFreshes var_names $ betas red e) $ \r -> do 
+                    elClass "li" "steps"  $ do
+                        text . pprintdb (maybe [] mmAssocs dbm) $ r
+                        b <- elClass "span" "up" $ button "edit"
+                        return (r, fmap (const r) b)
+            s <- record          
+            -- creation of the DMap ReductionE Identity tag :=> ES of the right type              
+            let     b = NewButton :=> fmap (flip (,) (last bs)) s
+                    e = NewEdit :=> leftmost es
+                    r' = ChangeTactic :=> r
+            return . Just . merge $ fromList [b,e,r']
 
 ----------------- an initial set of expressions ----------------
+--
+var_names = nub $ "xyzwnmlkij" ++ ['a' .. 'z']
+
 boot :: ButtonsDefs
 
-boot = foldr new M.empty $ map swap [
-    (false var_names,"FALSE"),
-    (true var_names,"TRUE"),
-    (and_ var_names,"AND"),
-    (or_ var_names,"OR"),
-    (id_ var_names,"ID"),
-    (zero var_names,"ZERO"),
-    (suc var_names,"SUCC"),
-    (plus var_names,"PLUS")
-    ]
+boot = foldr new M.empty $ [
+    ("FALSE",false var_names),
+    ("TRUE",true var_names),
+    ("AND",and_ var_names),
+    ("OR",or_ var_names),
+    ("ID",id_ var_names),
+    ("ZERO",zero var_names),
+    ("SUCC",suc var_names),
+    ("PLUS",plus var_names)
+    ] 
+
 --------------------- group button widget --------------------------
---
 
 
+-- expression elements -----------------------------------------
 extrabuttons :: MS m => m [ES String]
 extrabuttons = mapM (\c -> c <$$ button c) ["(" ,"(λ","x" ,"y" ,"z" ,"w" ,"n" ,"m" ,"l" ,"." ,")"] 
 
-
+-- make a button firing an expression
 makeButton :: MS m => String -> DS EC -> m (ES String)
 makeButton k v = fmap (pprint . fst) <$> attachDyn v <$> button k
 --
 buttonsW :: (MonadWidget Spider m) => DS ButtonsDefs -> m (ES String)
 buttonsW buttonsDef = divClass "standard" $ do 
-            keys <- extrabuttons
-            dynOfEvents <- listWithKey buttonsDef (makeButton . snd) >>= mapDyn (leftmost . M.elems)           
-            return . leftmost $ (switch . current $ dynOfEvents) : keys
+    
+    keys <- extrabuttons
+    dynOfEvents :: DS (ES String) <- listWithKey buttonsDef (makeButton . snd) >>= mapDyn (leftmost . M.elems)           
+    return . leftmost $ (switch . current $ dynOfEvents) : keys
 
-
---
 ----------------- the expression field widget ---------------
 --
--- expressionW :: MS m => ES String -> ES String -> m (DS String,  DS Bool)
+expressionW :: MS m => ES String -> ES EC -> m (DS String,  DS Bool)
 expressionW buttons picked = divClass "edit" $ do 
-                divClass "title" $ do
-                                    text "Expression"
-                c <- divClass "usenames" $ do
-                                    c <- checkbox False def
-                                    elClass "span" "tooltip" $ text "substitute names"
-                                    return c
-                b <- divClass "clear" $ button "clear"
-                (,view checkbox_value c) <$> divClass "expression" (selInputW buttons (fmap pprint picked) b)
+    divClass "title" $ text "Expression"
+    c <- divClass "usenames" $ do
+        c <- checkbox False def
+        elClass "span" "tooltip" $ text "substitute names"
+        return c
+    b <- divClass "clear" $ button "clear"
+    (,view checkbox_value c) <$> divClass "expression" (selInputW buttons (fmap pprint picked) b)
 
 
 ----------------- a dumb footer ---------------------------
 --
 footer :: MS m => m ()
-footer = divClass "edit" . divClass "tooltip" $ text "Ghcjs + Reflex application. Kudos to them!"
+footer = divClass "edit" . divClass "tooltip" $ do
+        linkNewTab "https://github.com/ghcjs/ghcjs" "GhcJs" 
+        text " + "
+        linkNewTab "https://github.com/reflex-frp/reflex-platform" "Reflex"
+        text " application. Kudos to them!"
 
 main = mainWidget . void $ do
 
